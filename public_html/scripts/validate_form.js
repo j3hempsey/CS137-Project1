4bb1cdeaff8ab5af7c9b449460ceff1564012728
Jed <jedgentry@mac.com>
2016-04-14 12:01:28 -0700

    Please help me

---
+++ public_html/scripts/validate_form.js
@@ -0,0 +1,94 @@
+function checkEmail(email)
+{
+	var regex = /^([a-zA-Z0-9_.-])+@(a-zA-Z0-9_.-)+\.(a-zA-Z])/;
+	return regex.test(email);
+}
+
+function checkName(name)
+{
+	var regex = /^([a-ZA-Z])/;
+	return regex.test(name);
+}
+
+function checkCard(creditCard)
+{
+	var regex = /^([0-9])/;
+	return (regex.test(name) && creditCard.length == 16)
+}
+
+function checkAddress(address)
+{
+	var regex = /^([0-9])+\ (a-ZA-Z)*/;
+	return regex.test(address);
+}
+
+function checkZip(zip)
+{
+	var regex = /^([0-9])/;
+	return (regex.test(zip) && regex.length == 6)
+}
+
+function checkState(state)
+{
+	var regex = /^([a-zA-Z]/;
+	return (regex.test(state) && regex.length == 2)
+}
+
+function checkQuantity(quantity)
+{
+	var regex = /^([0-9])/;
+	return regex.test(quantity);
+}
+
+function processForm()
+{
+	var first_name = document.getElementById('first-name');
+	var last_name = document.getElementById('last-name');
+	var credit_card = document.getElementById('credit-card');
+	var address = document.getElementById('address');
+	var zip = document.getElementById('zip-code');
+	var state = document.getElementById('state');
+	var quantity = document.getElementById('quantity');
+	var error_message = 'Errors in the following fields: \n\n';
+	var error = false;
+	
+	if(!checkName(first_name))
+	{
+		error_message += 'First name\n'
+		error = true;
+	}
+	if(!checkName(last_name))
+	{
+		error_message += 'Last name\n'
+		error = true;
+	}
+	if(!checkCard(credit_card))
+	{
+		error_message += 'Credit Card\n'
+		error = true;
+	}
+	if(!checkAddress(address))
+	{
+		error_message += 'Address\n'
+		error = true;
+	}
+	if(!checkZip(zip))
+	{
+		error_message += 'Zip\n'
+		error = true;
+	}
+	if(!checkState(state))
+	{
+		error_message += 'State\n'
+		error = true;
+	}
+	if(!checkQuantity(quantity))
+	{
+		error_message += 'Quantity\n'
+		error = true;
+	}
+	if(error)
+	{
+		alert(error_message);
+	}
+}
